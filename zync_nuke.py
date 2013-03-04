0bb66213efe46961abd933ada4b62c515ce623be
Brian Cipriano <bcipriano@zyncrender.com>
2013-03-04 13:21:14 -0500

    Disabled Nuke script cleaning when using Nuke 7. nuke.Undo.cancel() appears to be broken in 7, which causes cleaning changes to stick around. The user could easily save those changes to their real script. Will re-enable when we find another way to do it, or when Foundry fixes this bug.

--- zync_nuke.py
+++ zync_nuke.py
@@ -574,22 +574,27 @@ class ZyncRenderPanel(nukescripts.panels.PythonPanel):
         else:
             viewer_input, viewed_node = None, None
 
-        new_script = generate_script_path()
-        with WriteChanges(new_script):
-            # The WriteChanges context manager allows us to save the
-            # changes to the current session to the given script, leaving
-            # the current session unchanged once the context manager is
-            # exited.
+        if nuke.NUKE_VERSION_MAJOR < 7:
+            new_script = generate_script_path()
+            with WriteChanges(new_script):
+                # The WriteChanges context manager allows us to save the
+                # changes to the current session to the given script, leaving
+                # the current session unchanged once the context manager is
+                # exited.
+                preflight_result = preflight()
+                select_deps( selected_write_nodes )
+
+                for node in nuke.allNodes():
+                    if node.isSelected():
+                        node.setSelected(False)
+                    else:
+                        node.setSelected(True)
+                nuke.nodeDelete()
+
+        else:
+            new_script = nuke.root().knob('name').getValue()
             preflight_result = preflight()
-            select_deps( selected_write_nodes )
-
-            for node in nuke.allNodes():
-                if node.isSelected():
-                    node.setSelected(False)
-                else:
-                    node.setSelected(True)
-            nuke.nodeDelete()
-
+            
         if not preflight_result:
             return
 
